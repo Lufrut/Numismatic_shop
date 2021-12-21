c8f28ecd17c4ffe59ee219a531c19a3ea5fe14e2
Lufrut <56224664+Lufrut@users.noreply.github.com>
2021-12-22 00:28:48 +0200

    add editing&delete account

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -41,46 +41,73 @@ class UserController {
         const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
     }
-    async addCoinToBasket(req,res,next){
-        const{coinId} = req.body
-        const basket = await Basket.findOne({where:{userId:req.user.id}})
-        const checkCoinInBasket = await BasketCoin.findOne({where:{coinId,basketId:basket.id}})
-        if(checkCoinInBasket){
-        return next(ApiError.badRequest("Товар уже в корзине!"))
-        }
-       await BasketCoin.create({
-           basketId:basket.id,
-           coinId,
-           count:1,
-       })
-    }
-    async editCoinBasketCount(req,res,next){
-        const{coinId,count} = req.body
-        const basket = await Basket.findOne({where:{userId:req.user.id}})
-        const coinBasket = await BasketCoin.findOne({where:{coinId,basketId:basket.id}})
-        if(!coinBasket){
+
+    async addCoinToBasket(req, res, next) {
+        const {coinId} = req.body
+        const basket = await Basket.findOne({where: {userId: req.user.id}})
+        const checkCoinInBasket = await BasketCoin.findOne({where: {coinId, basketId: basket.id}})
+        if (checkCoinInBasket) {
             return next(ApiError.badRequest("Товар уже в корзине!"))
         }
-        await BasketCoin.update({count},{where:{id}})
+        await BasketCoin.create({
+            basketId: basket.id,
+            coinId,
+            count: 1,
+        })
+    }
+
+    async editCoinBasketCount(req, res, next) {
+        const {coinId, count} = req.body
+        const basket = await Basket.findOne({where: {userId: req.user.id}})
+        const coinBasket = await BasketCoin.findOne({where: {coinId, basketId: basket.id}})
+        if (!coinBasket) {
+            return next(ApiError.badRequest("Товар уже в корзине!"))
+        }
+        await BasketCoin.update({count}, {where: {id}})
         return res.statusCode(200);
     }
-    async getBasketContent(req,res,next){
+
+    async getBasketContent(req, res, next) {
         let {limit, page} = req.query
-        const basket = await Basket.findOne({where:{userId:req.user.id}})
-        const coinBasket = await BasketCoin.findOne({where:{basketId:basket.id}})
-        if(!coinBasket){
+        const basket = await Basket.findOne({where: {userId: req.user.id}})
+        const coinBasket = await BasketCoin.findOne({where: {basketId: basket.id}})
+        if (!coinBasket) {
             return next(ApiError.badRequest("Товар уже в корзине!"))
         }
         page = page || 1
         limit = limit || 9
         let offset = page * limit - limit
-        const content = await BasketCoin.findAndCountAll({where:{basketId:basket.id}, limit, offset})
+        const content = await BasketCoin.findAndCountAll({where: {basketId: basket.id}, limit, offset})
         return res.json(content)
     }
+
     async check(req, res, next) {
         const token = generateJwt(req.user.id, req.user.email, req.user.role)
         return res.json({token})
     }
+    async deleteUserAccount(req,res){
+        const {id} = req.user
+        await User.destroy({
+            where: {id: id}
+        })
+    }
+    async changePassword(req, res,next) {
+        const {password,old_password} = req.body
+        const {id} = req.user
+        const old_hash_password = await bcrypt.hash(old_password,5)
+        const user = await User.findOne({where:{id:id,password:old_hash_password}})
+        if(!user){
+            next(ApiError.badRequest("Password is wrong"))
+        }
+        const hash_password = await bcrypt.hash(password,5)
+        await User.update(
+            {
+                password:hash_password
+            },
+            {
+                where: {id: id}
+            })
+        return res.status(200).json("OK");
+    }
 }
-
 module.exports = new UserController()
